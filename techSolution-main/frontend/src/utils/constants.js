3c489d9edd3922adfe6ad7c8a35b8256a8205cb4
visu sarvaiya <visusarvaiya54@gamil.com>
2026-04-01 15:39:00 +0530

    change in frontend

--- techSolution-main/frontend/src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-// Application constants
-export const COMPANY_NAME = 'Tech Solution';
-export const COMPANY_LOGO = '/logo.png'; // You can add your logo later
-
-export const CURRENCY_SYMBOL = '₹';
-export const CURRENCY_CODE = 'INR';
-
